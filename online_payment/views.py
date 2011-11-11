bacea1fa60e12db572686f948dce794ac3d9069f
Shlok <shlokch@iitrpr.ac.in>
2011-11-11 20:48:18 +0530

    Account num validated

--- online_payment/views.py
+++ online_payment/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render_to_response
 from online_payment.models import Tax_transaction,BackOffice_dummy,Accounts_dummy,Online_Transaction_dummy,Taxtype
 import datetime
@@ -14,12 +14,15 @@ def detail(request):
 	        q = request.GET['accno']
 		ttype=request.GET['type']
 		Type=Taxtype.objects.get(tax_type_id=ttype)
-	        custac = Accounts_dummy.objects.get(account_number=q)
-		custinfo=BackOffice_dummy.objects.get(customer_id=custac.customer_id_id)
+		try:
+	        	custac = Accounts_dummy.objects.get(account_number=q)
+			custinfo=BackOffice_dummy.objects.get(customer_id=custac.customer_id_id)
+		except:
+			return HttpResponseRedirect("/onlinePayment/") 		
 		return render_to_response('ttype1.html',locals())		
 		#return render_to_response('ttype1.html',{'custac':ac , 'custinfo': cust})
     	else:
-        	return HttpResponse('Please enter Valid accno')
+        	return HttpResponseRedirect('/onlinePayment/')
 def confirm_payment(request):
 	if 'amount' in request.GET and request.GET['amount']:
 		a=request.GET['amount']
@@ -57,7 +60,7 @@ def onl_validatin(request):
 			custac.save()
 			return render_to_response('done.html',locals())
 		else:
-			return render_to_response('confirm.html')
+			return HttpResponseRedirect('homePage2/')
 	else:
 		return HttpResponse('Please enter Amount')
 
